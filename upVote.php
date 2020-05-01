441d05a0d7b9849b3fb3d022cef0442f53908025
katetrim <61316432+katetrim@users.noreply.github.com>
2020-05-01 18:18:02 +0100

    Add files via upload

---
+++ upVote.php
@@ -0,0 +1,59 @@
+<?php
+include 'includes/db.inc.php';
+session_start();
+
+$user = $_SESSION['UserID'];
+if($_POST['id'])
+{
+$id=$_POST['id'];
+$id = mysqli_real_escape_string($conn, $id);
+$ip_sql=mysqli_query($conn, "SELECT VoteID from votes WHERE FeedbackId ='$id' and UserID='$user'");
+    $count=mysqli_num_rows($ip_sql);
+    
+    if($count==0)
+    
+  {  
+if($_POST['type']=='like'){
+$sql = "UPDATE feedback SET upVote = upVote+1 WHERE FeedbackID='$id'";
+mysqli_query($conn,$sql);
+    
+    $sql_vote="INSERT INTO votes (FeedbackId, UserID) values ('$id', '$user')"; 
+    mysqli_query($conn,$sql_vote);
+    
+
+
+
+
+   
+ 
+
+    
+
+} else if($_POST['type']=='dislike')
+{
+    $sql2 = "UPDATE feedback SET upVote = upVote-1 WHERE FeedbackID='$id'";
+mysqli_query($conn,$sql2);
+    $sql_vote2="INSERT INTO votes (FeedbackId, UserID) values ('$id', $user)";
+    mysqli_query($conn,$sql_vote2);
+    
+
+
+
+    
+ 
+}
+
+    
+    }
+}
+else
+{
+   
+}
+$result=mysqli_query($conn, "SELECT upVote  from feedback where FeedbackID='$id'");
+$row=mysqli_fetch_array($result);
+ $vote = $row['upVote'];
+  echo $vote;
+
+
+?>
